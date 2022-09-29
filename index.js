042a29daa63c6f92d939239881b554adeff9bca1
paullaster <paullasterokoth98@gmail.com>
2022-09-29 06:28:48 +0300

    word scrambler func

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * Create a function that returns a value of a word and scrambles 
+ * the letter order with Math.random():
+ */
+const word_scrambler = (str) => {
+    let len = str.length;
+    console.log(len);
+};
+ word_scrambler('this is a word');
